8508877c2d364f549549bea57d24cd524be28831
AVVS <v@aminev.me>
2016-01-08 19:33:29 +0300

    fix(travis) - add an encrypted archive

--- deploy/docean.sh
+++ deploy/docean.sh
@@ -4,10 +4,9 @@ docker login --email="$DOCKER_EMAIL" --username="$DOCKER_LOGIN" --password="$DOC
 make push
 
 eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_23bd8dcc3242_key -iv $encrypted_23bd8dcc3242_iv -in ./crypto/id_rsa.enc -out ./id_rsa.pem -d
-chmod 400 ./id_rsa.pem
+chmod 400 ./id_rsa
 echo $SSH_PWD > .echo_ps && chmod 700 .echo_ps
-cat ./id_rsa.pem | SSH_ASKPASS=.echo_ps ssh-add -
+cat ./id_rsa | SSH_ASKPASS=.echo_ps ssh-add -
 rm .echo_ps
 ssh $DEPLOY_USER@$DEPLOY_HOST "
 docker login --email=\"$DOCKER_EMAIL\" --username=\"$DOCKER_LOGIN\" --password=\"$DOCKER_PWD\"
