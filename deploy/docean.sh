9a89f3f34263422d00f497351d9bdfe74582e108
AVVS <v@aminev.me>
2016-01-08 20:59:24 +0300

    fix: add scp of docker-compose file

--- deploy/docean.sh
+++ deploy/docean.sh
@@ -8,6 +8,7 @@ chmod 400 ./id_rsa
 echo $SSH_PWD > .echo_ps && chmod 700 .echo_ps
 cat ./id_rsa | SSH_ASKPASS=.echo_ps ssh-add -
 rm .echo_ps
+scp ./docker-compose.yml $DEPLOY_USER@$DEPLOY_HOST:~/docker-compose.yml
 ssh $DEPLOY_USER@$DEPLOY_HOST "
 docker login --email=\"$DOCKER_EMAIL\" --username=\"$DOCKER_LOGIN\" --password=\"$DOCKER_PWD\"
 docker-compose pull
