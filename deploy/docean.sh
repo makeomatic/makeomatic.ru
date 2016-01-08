fca68fde482ef91593fcf3f4fc593adcd8a363e0
AVVS <v@aminev.me>
2016-01-08 22:01:20 +0300

    move push to script stage

--- deploy/docean.sh
+++ deploy/docean.sh
@@ -5,9 +5,6 @@ function cleanup {
 }
 trap cleanup EXIT
 
-docker login --email="$DOCKER_EMAIL" --username="$DOCKER_LOGIN" --password="$DOCKER_PWD"
-make push || exit 1
-
 openssl rsa -in ./id_rsa -out ./id_rsa.insecure -passin pass:$SSH_PWD
 chmod 0400 ./id_rsa.insecure
 scp -i ./id_rsa.insecure ./docker-compose.yml $DEPLOY_USER@$DEPLOY_HOST:~/docker-compose.yml
