6fc07e9b5d1ec07bcff2ac4f120c0f0c29cccf35
AVVS <v@aminev.me>
2016-01-08 22:16:48 +0300

    fix: escape $ in deploy script

--- deploy/docean.sh
+++ deploy/docean.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 function cleanup {
-  rm -f ./id_rsa ./id_rsa.insecure
+  rm -f ./id_rsa ./id_rsa.insecure ~/.docker/config.json
 }
 trap cleanup EXIT
 
 openssl rsa -in ./id_rsa -out ./id_rsa.insecure -passin pass:$SSH_PWD
 chmod 0400 ./id_rsa.insecure
+
+echo "copying docker-compose file via scp"
 scp -i ./id_rsa.insecure ./docker-compose.yml $DEPLOY_USER@$DEPLOY_HOST:~/docker-compose.yml
+
+echo "running ssh-exec"
 ssh -i ./id_rsa.insecure $DEPLOY_USER@$DEPLOY_HOST "
 docker login --email=\"$DOCKER_EMAIL\" --username=\"$DOCKER_LOGIN\" --password=\"$DOCKER_PWD\"
 docker-compose pull
 docker-compose up -d
-docker rmi $(docker images -f 'dangling=true' -q)
+docker rmi $$(docker images -f 'dangling=true' -q)
 "
