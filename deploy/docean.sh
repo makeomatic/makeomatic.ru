6e5e0a5d246a7de6bfb066af132bddd2647d77a8
AVVS <v@makeomatic.ru>
2017-04-15 22:42:19 -0700

    upgrade to letsencrypt

--- deploy/docean.sh
+++ deploy/docean.sh
@@ -11,6 +11,9 @@ chmod 0400 ./id_rsa.insecure
 echo "copying docker-compose file via scp"
 scp -i ./id_rsa.insecure ./docker-compose.yml $DEPLOY_USER@$DEPLOY_HOST:~/docker-compose.yml
 
+echo "copy nginx tmpl"
+scp -i ./id_rsa.insecure ./nginx.tmpl $DEPLOY_USER@$DEPLOY_HOST:~/nginx.tmpl
+
 echo "running ssh-exec"
 ssh -i ./id_rsa.insecure $DEPLOY_USER@$DEPLOY_HOST "
 docker login --username=\"$DOCKER_LOGIN\" --password=\"$DOCKER_PWD\"
